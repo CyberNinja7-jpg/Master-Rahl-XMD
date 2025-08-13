1258077e28d5b7f43e812e81e4424aff8503f840
CyberNinja7-jpg <albertnyasimi27@gmail.com>
2025-08-13 08:49:11 +0300

    Update mesfonctions.js

--- Ibrahim/mesfonctions.js
+++ Ibrahim/mesfonctions.js
@@ -32,7 +32,7 @@ module.exports.genererNomFichier = async (extension) => {
 /** ************ */
 module.exports.stick = async (buffer, author) => {
     var sticker = new Sticker(buffer, {
-        pack: 'BMW-MD',
+        pack: 'LORD RAHL-MD',
         author: author,
         type: StickerTypes.FULL,
         categories: ['🤩', '🎉'],
